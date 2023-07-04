f16676493d286eb695fa9cbc6b2d01f5b8b41ea1
bli farda <134302929+farda222@users.noreply.github.com>
2023-07-04 13:56:25 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#02080B',
+      },
+      zIndex: {
+        'top': '1',
+      },
+    },
+  },
+  plugins: [],
+}
+
